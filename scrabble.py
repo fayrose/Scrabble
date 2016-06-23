eff13718ec3a818c922272e374196b61cd58f01e
Lauren Fay <laurenfay.rosenberg@mail.utoronto.ca>
2016-06-23 09:56:38 -0400

    Added comments to enhance readability.

--- scrabble.py
+++ scrabble.py
@@ -16,24 +16,41 @@ Board - keeps track of the tiles' location on the board
 """
 
 class Tile:
+    """
+    Class that allows for the creation of a tile. Initializes using an uppercase string of one letter,
+    and an integer representing that letter's score.
+    """
     def __init__(self, letter, score):
-        self.letter = letter
+        self.letter = letter.upper()
         self.score = score
+
     def __str__(self):
         return self.letter
+
+    def get_score():
+        #Returns the tile's score value.
+        return self.score
+
 class Bag:
+    """
+    Creates the bag of all tiles that will be available during the game. Contains 98 letters and two blank tiles.
+    Takes no arguments to initialize.
+    """
     def __init__(self):
         self.bag = []
+        self.initialize_bag()
 
     def __str__(self):
         for line in self.bag:
             "\n"
 
     def add_to_bag(self, tile, quantity):
+        #Adds a certain quantity of a certain tile to the bag. Takes a tile and an integer quantity as arguments.
         for i in range(quantity):
             self.bag.append(tile)
 
     def initialize_bag(self):
+        #Adds the intiial 100 tiles to the bag.
         self.add_to_bag(Tile("A", 1), 9)
         self.add_to_bag(Tile("B", 3), 2)
         self.add_to_bag(Tile("C", 3), 2)
@@ -61,57 +78,94 @@ class Bag:
         self.add_to_bag(Tile("Y", 4), 2)
         self.add_to_bag(Tile("Z", 10), 1)
         shuffle(self.bag)
+
     def take_from_bag(self):
+        #Removes a tile from the bag and returns it to the user. This is used for replenishing the rack.
         return self.bag.pop()
 
+    def get_remaining_tiles(self):
+        #Returns the number of tiles left in the bag.
+        return len(self.bag)
+
 class Rack:
+    """
+    Creates each player's 'dock', or 'hand'. Allows players to add, remove and replenish the number of tiles in their hand.
+    """
     def __init__(self, bag):
         self.rack = []
         self.initialize(bag)
 
     def add_to_rack(self, bag):
+        #Takes a tile from the bag and adds it to the player's rack.
         self.rack.append(bag.take_from_bag())
 
     def initialize(self, bag):
+        #Adds the initial 7 tiles to the player's hand.
         for i in range(7):
             self.add_to_rack(bag)
 
     def get_rack_str(self):
+        #Displays the user's rack in string form.
         return ", ".join(str(item) for item in self.rack)
 
     def get_rack_arr(self):
+        #Returns the rack in the form of an array.
         return self.rack
+
+    def remove_from_rack(self, tile):
+        #Removes a tile from the rack (for example, when a tile is being played).
+        self.rack.remove(tile)
+
+    def get_rack_length(self):
+        #Returns the number of tiles left in the rack.
+        return len(self.rack)
+
+    def replenish_rack(self):
+        #Adds tiles to the rack after a turn such that the rack will have 7 tiles (assuming a proper number of tiles in the bag).
+        while self.get_rack_length < 7:
+            self.add_to_rack(bag)
+
 class Player:
+    """
+    Creates an instance of a player. Initializes the player's rack, and allows you to set/get a player name.
+    """
     def __init__(self, bag):
         self.name = ""
         self.rack = Rack(bag)
 
     def set_name(self, name):
+        #Sets the player's name.
         self.name = name
 
     def get_name(self):
+        #Gets the player's name.
         return self.name
 
     def get_rack(self):
+        #Returns the player's rack.
         return self.rack.get_rack_str()
 
 class Board:
+    """
+    Creates the scrabble board.
+    """
     def __init__(self):
         self.board = [[" 0 " for i in range(15)] for j in range(15)]
         self.add_premium_squares()
         self.board[7][7] = " * "
 
     def get_board(self):
-        #Create evenly spaced table!
-        board_str = ""
+        #Returns the board in string form.
+        board_str = "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n"
         board = list(self.board)
         for i in range(len(board)):
             board[i] ="| " + " | ".join(str(item) for item in board[i]) + " |"
-        board_str = "\n|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|\n".join(board)
-
+        board_str += "\n|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|\n".join(board)
+        board_str += "\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _"
         return board_str
 
     def add_premium_squares(self):
+        #Adds all of the premium squares that influence the word's score.
         TRIPLE_WORD_SCORE = ((0,0), (7, 0), (14,0), (0, 7), (14, 7), (0, 14), (7, 14), (14,14))
         DOUBLE_WORD_SCORE = ((1,1), (2,2), (3,3), (4,4), (1, 13), (2, 12), (3, 11), (4, 10), (13, 1), (12, 2), (11, 3), (10, 4), (13,13), (12, 12), (11,11), (10,10))
         TRIPLE_LETTER_SCORE = ((1,5), (1, 9), (5,1), (5,5), (5,9), (5,13), (9,1), (9,5), (9,9), (9,13), (13, 5), (13,9))
@@ -127,6 +181,7 @@ class Board:
             self.board[coordinate[0]][coordinate[1]] = "DLS"
 
     def play_word(self, word, location, direction):
+        #Allows you to play words, assuming that they have already been confirmed as valid.
         direction.lower()
         word = word.upper()
         if direction == "right":
@@ -137,10 +192,11 @@ class Board:
                 self.board[location[0]+i][location[1]] = " " + word[i] + " "
         else:
             print("Error: please enter a valid direction.")
+        #Remove letters from rack
 
 def check_word(word, location, player):
+    #Checks the word to make sure that it is in the dictionary, and that the location falls within bounds.
     word = word.upper()
-    #Check that the word is in the dictionary
     dictionary = open("dic.txt").read()
     if word not in dictionary:
         return "Please enter a valid dictionary word."
@@ -152,27 +208,40 @@ def check_word(word, location, player):
     return True
 
 def turn(player, board):
-    global round_number
+    #Begins a turn, by displaying the current board, getting the information to play a turn, and creates a recursive loop to allow the next person to play.
+    global round_number, player1, player2
     print("Round " + str(round_number) + ": " + player.get_name() + "'s turn \n")
     print(board.get_board())
     print("\n" + player.get_name() + "'s Letter Rack: " + player.rack.get_rack_str())
     word_to_play = raw_input("Word to play: ")
-    location = raw_input("Location of first letter: (Please separate coordinates with a comma)")
+    location = []
+    location.append(int(raw_input("Column number: "))
+    location.append(int(raw_input("Row number: ")))
     direction = raw_input("Direction of word (right or down): ")
     while (check_word(word_to_play, location, player)) != True:
-        word_to_play = raw_input("Word to play: ")
-        location = raw_input("Location of first letter: ")
-        direction = raw_input("Direction of word (right or down): ")
         print (check_word(word_to_play, location, player))
+        word_to_play = raw_input("Word to play: ")
+        location = []
+        location.append(int(raw_input("Column number: "))
+        location.append(int(raw_input("Row number: ")))
+        direction = raw_input("Direction of word (right or down): ")
     board.play_word(word_to_play, tuple(map(int,location.split(','))), direction)
     print(board.get_board())
+    if player == player1:
+        player = player2
+        turn(player, board)
+    elif player == player2:
+        player = player1
+        round_number += 1
+        turn(player, board)
+    #Implement scoring system
 
 def start_game():
-    global round_number
+    #Begins the game and calls the turn function.
+    global round_number, player1, player2
     board = Board()
 
     bag = Bag()
-    bag.initialize_bag()
 
     player1 = Player(bag)
     player2 = Player(bag)
@@ -185,4 +254,5 @@ def start_game():
     print("Welcome " + player1.get_name() + " and " + player2.get_name() + "! Let's begin!")
 
     turn(current_player, board)
+
 start_game()
